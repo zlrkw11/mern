dfcfb145921197d9512b8cc67b854071829ed1b5
ZL <ray040406@gmail.com>
2024-11-17 17:32:49 +1300

    dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
+require("dotenv").config();
+
 const express = require("express");
+
+// express app
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ mssg: "Welcome to the app" });
+});
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+  console.log("listening on port 4000!");
+});
