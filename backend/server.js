c410679ebc48af7207dc6e3f618bbe90430a0f39
ZL <ray040406@gmail.com>
2024-11-17 18:48:02 +1300

    x

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,11 @@ const express = require("express");
 // express app
 const app = express();
 
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.json({ mssg: "Welcome to the app" });
 });
