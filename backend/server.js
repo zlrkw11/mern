11f0c87fa4e4ac3f8c363e5981fb1df63af2ce7e
ZL <ray040406@gmail.com>
2024-11-18 14:02:08 +1300

    connect to db

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 // express app
 const app = express();
@@ -12,6 +13,14 @@ app.use((req, res, next) => {
 
 // attach all routes to app
 app.use("/api/workouts", workoutRoutes);
+
+//connect to db
+mongoose
+  .connect("process.env.MONGO_URI")
+  .then(() => {})
+  .catch((error) => {
+    console.log(error);
+  });
 
 // listen for requests
 app.listen(process.env.PORT, () => {
