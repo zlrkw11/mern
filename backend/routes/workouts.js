6054d4a6b56ff6559679f5ec460ea68d1bf42420
ZL <ray040406@gmail.com>
2024-11-21 00:04:03 +1300

    imported routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const Workout = require("../models/workoutModel");
 const router = express.Router();
-const { createWorkout } = require("../controllers/workoutController");
+const {
+  createWorkout,
+  getWorkouts,
+  getWorkout,
+} = require("../controllers/workoutController");
 // GET all workouts
-router.get("/", (req, res) => {
-  res.json({ mssg: "GET all workouts" });
-});
+router.get("/", getWorkouts);
 
 // GET a single workout
-router.get("/:id", (req, res) => {
-  res.json({ mssg: "GET a single workout" });
-});
+router.get("/:id", getWorkout);
 
 // POST a new workout
 router.post("/", createWorkout);
