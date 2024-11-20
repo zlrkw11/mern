1cdee65d5dc991c125767145035c32c083de73a7
ZL <ray040406@gmail.com>
2024-11-21 00:03:02 +1300

    get all workouts

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,9 +1,20 @@
 const Workout = require("../models/workoutModel");
 
 // get all workouts
+const getWorkouts = async (req, res) => {
+  const workouts = await Workout.find({}).sort({ createdAt: -1 });
+  res.status(200).json(workouts);
+};
 
 // get a single workout
-
+const getWorkout = async (req, res) => {
+  const { id } = req.params;
+  const workout = await Workout.findById(id);
+  if (!workout) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+  res.status(200).json(workout);
+};
 // create new workout
 const createWorkout = async (req, res) => {
   const { title, load, reps } = req.body;
@@ -25,5 +36,6 @@ const createWorkout = async (req, res) => {
 // update a workout
 
 module.exports = {
+  getWorkouts,
   createWorkout,
 };
