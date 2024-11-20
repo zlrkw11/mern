631c6b0392a43023412800d08f35ea83c2469b6e
ZL <ray040406@gmail.com>
2024-11-20 23:55:37 +1300

    create a workout

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -5,6 +5,20 @@ const Workout = require("../models/workoutModel");
 // get a single workout
 
 // create new workout
+const createWorkout = async (req, res) => {
+  const { title, load, reps } = req.body;
+
+  try {
+    const workout = await Workout.create({
+      title,
+      load,
+      reps,
+    });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 // delete a workout
 
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -13,19 +13,7 @@ router.get("/:id", (req, res) => {
 });
 
 // POST a new workout
-router.post("/", async (req, res) => {
-  const { title, load, reps } = req.body;
-  try {
-    const workout = await Workout.create({
-      title,
-      load,
-      reps,
-    });
-    res.status(200).json(workout);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/", async (req, res) => {});
 
 // DELETE a workout
 router.delete("/:id", (req, res) => {
