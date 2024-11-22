ce27be734cd954c154848563735253d104f17b1a
ZL <ray040406@gmail.com>
2024-11-22 15:44:39 +1300

    proxy

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   const [workouts, setWorkouts] = useState(null);
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch("http://localhost:4000/api/workouts");
+      const response = await fetch("/api/workouts");
       const json = await response.json();
       if (response.ok) {
         setWorkouts(json);
