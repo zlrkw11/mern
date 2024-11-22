580aae676cfe8967ee7054a68179475b9ca5ff7a
ZL <ray040406@gmail.com>
2024-11-22 15:38:55 +1300

    async fetching function

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,14 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("http://localhost:4000/api/workouts");
+    };
+
+    fetchWorkouts();
+  }, []);
+
   return (
     <div className="home">
       <h2>Home</h2>
