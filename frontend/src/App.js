3cc93d16e68c15fdd57a5cad3f2210472fa0e110
ZL <ray040406@gmail.com>
2024-11-22 15:29:43 +1300

    router

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route path='/' element={}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
