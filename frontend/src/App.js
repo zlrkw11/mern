f8a3d2d74295a2cf1d1c52dc3dec9c4344c600f6
ZL <ray040406@gmail.com>
2024-11-22 15:35:58 +1300

    paste in css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-
+import Navbar from "./components/navbar";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
