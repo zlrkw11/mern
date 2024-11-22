bc48844ae0dada4fc851bafb37ce1d9b721673e6
ZL <ray040406@gmail.com>
2024-11-22 15:41:04 +1300

    check if ok, then set to json array of objects

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
   useEffect(() => {
     const fetchWorkouts = async () => {
       const response = await fetch("http://localhost:4000/api/workouts");
+      const json = await response.json();
+      if (response.ok) {
+        setWorkouts(json);
+      }
     };
-
     fetchWorkouts();
   }, []);
 
