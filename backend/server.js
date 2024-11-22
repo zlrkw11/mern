7f4d26cbcc906a24adbad2a9c1575fd294a9d9f8
ZL <ray040406@gmail.com>
2024-11-23 00:31:16 +1300

    added middleware

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 // express app
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
