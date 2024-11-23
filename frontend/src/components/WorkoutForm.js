39e265d072143422c5ba1b70251de37a54f3b941
ZL <ray040406@gmail.com>
2024-11-23 18:26:04 +1300

    handle submit function

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -3,9 +3,12 @@ const WorkoutForm = () => {
   const [title, setTitle] = useState("");
   const [load, setLoad] = useState("");
   const [reps, setReps] = useState("");
-
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const workout = { title, load, reps };
+  };
   return (
-    <form className="create">
+    <form className="create" onSubmit={handleSubmit}>
       <h3>Add a new workout</h3>
       <label>Exercise title</label>
       <input
@@ -13,18 +16,22 @@ const WorkoutForm = () => {
         onChange={(e) => setTitle(e.target.value)}
         value={title}
       ></input>
+
       <label>Load (kg)</label>
       <input
         type="text"
         onChange={(e) => setLoad(e.target.value)}
         value={load}
       ></input>
+
       <label>Reps</label>
       <input
         type="text"
         onChange={(e) => setReps(e.target.value)}
         value={reps}
       ></input>
+
+      <button>Add Workout</button>
     </form>
   );
 };
