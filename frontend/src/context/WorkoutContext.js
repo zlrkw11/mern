80a8e610432ee32054293436ad6f3c4e07e0ee0c
ZL <ray040406@gmail.com>
2024-11-24 18:49:22 +1300

    wraps the root using provider

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -1,3 +1,7 @@
 import { createContext } from "react";
 
 export const WorkoutsContext = createContext();
+
+export const WorkoutsContextProvider = ({ children }) => {
+  return <WorkoutsContext.Provider>{children}</WorkoutsContext.Provider>;
+};
